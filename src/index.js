7912e405fab39ba24fa8f731bbee121bef18b0f3
ogustm <osward.gust.martinez@gmail.com>
2019-11-12 13:53:33 +0100

    added csv and xml loader in webpack.config.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import _ from 'lodash';
 import './style.css';
+import Heart from './logo.gif'
 
 function component() {
     const element = document.createElement('div');
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack', 'test'], '');
     element.classList.add('hello');
+
+    //Add the image to our existing div.
+    const myIcon = new Image();
+    myIcon.src = Heart;
+    element.appendChild(myIcon);
 
     return element;
 }
